cb4d4988cc3fd378fe4dbf1257a47e2bfcdb618d
Clifford Aduako Donkor <clifforddonk@gmail.com>
2025-12-07 20:46:38 +0000

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,19 @@ import "./globals.css";
 import Providers from "./providers";
 
 const nunito = Nunito({
-  variable: "--font-nunito",
   subsets: ["latin"],
   weight: ["400", "700"],
 });
+
 export const metadata: Metadata = {
   title: "AirForce Quiz App",
   description: "An Online Quiz System",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${nunito.variable} antialiased`}>
+      <body className={`${nunito.className} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
